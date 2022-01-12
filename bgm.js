0fad20b82c79a39c146ccda59acce605fcfcc1e4
NegicLegend <88082310+NegicLegend@users.noreply.github.com>
2022-01-12 18:15:10 +0700

    fix bgm.js

--- bgm.js
+++ bgm.js
@@ -3,23 +3,34 @@ const toggleVolume = document.querySelector('.toggle-volume');
 var a = 0;
 audio.volume = 0.2;
 
-if(a === 0) {
-   audio.addEventListener('play', audio.pause());
-}
-
 audio.addEventListener('ended', audio.play());
 
 toggleVolume.onclick = function() {
-   a += 1;
    if(toggleVolume.classList.contains('active')) {
       toggleVolume.innerHTML = '<i class="icon fas fa-volume-mute"></i>'
       toggleVolume.classList.remove('active');
       toggleVolume.classList.add('mute');
       audio.pause();
    }else {
-      toggleVolume.innerHTML = '<i class="icon fas fa-volume-up"></i>'
-      toggleVolume.classList.remove('mute');
-      toggleVolume.classList.add('active');
-      audio.play();
+      var promise = new Promise(
+         function(resolve, reject) {
+            resolve()
+         }
+      );
+
+      promise
+         .then(function() {
+            if(a === 0) {
+               document.querySelector('.music').innerHTML = <audio id="audio" src="./bgm/introduce_bgm.mp3"></audio>
+            }
+         })
+         .then(function() {
+            toggleVolume.innerHTML = '<i class="icon fas fa-volume-up"></i>'
+            toggleVolume.classList.remove('mute');
+            toggleVolume.classList.add('active');
+            audio.play();
+         })
+         .catch(function() {})
    }
+   a += 1;
 }
