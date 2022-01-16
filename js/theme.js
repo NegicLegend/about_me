56bbc212953af219cd6ef990de1867a3d329238e
NegicLegend <88082310+NegicLegend@users.noreply.github.com>
2022-01-17 00:04:13 +0700

    add name animation

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,27 @@
-(function() {
+(function () {
    const theme = document.getElementById('theme');
    const changeThemeBtn = document.querySelector('.toggle-theme');
 
-   changeThemeBtn.onclick = function() {
-      if(theme.href.indexOf('dark') > -1) {
+   changeThemeBtn.onclick = function () {
+      if (theme.href.indexOf('dark') > -1) {
          theme.href = './theme/light.css';
          changeThemeBtn.innerHTML = '<i class="icon fas fa-sun"></i>';
-      }else if(theme.href.indexOf('light') > -1) {
+      } else if (theme.href.indexOf('light') > -1) {
          theme.href = './theme/dark.css';
          changeThemeBtn.innerHTML = '<i class="icon fas fa-moon"></i>';
-      }else {
+      } else {
          console.error('Something must be wrong!');
          theme.href = './theme/dark.css';
          changeThemeBtn.innerHTML = '<i class="icon fas fa-moon"></i>';
       }
+
+      (function() {
+         document.getAnimations().forEach((anim) => {
+            if(anim.animationName != 'MoveTop') {
+               anim.cancel();
+               anim.play();
+            }
+         });
+      })()
    }
 })()
