e721ae38cd5f58f93a0b561c6aab89aea61323f8
NegicLegend <88082310+NegicLegend@users.noreply.github.com>
2022-01-19 11:27:17 +0700

    change face (avt) color

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,15 @@
 (function () {
    const theme = document.getElementById('theme');
    const changeThemeBtn = document.querySelector('.toggle-theme');
-   var light = new File('./theme/light.css');
-   var dark = new File('./theme/dark.css');
 
    changeThemeBtn.onclick = function () {
       if (theme.href.indexOf('dark') > -1) {
-         if(light.exists()) {
-            theme.href = './theme/light.css';
-            changeThemeBtn.innerHTML = '<i class="icon fas fa-sun"></i>';  
-         }
+         theme.href = './theme/light.css';
+            changeThemeBtn.innerHTML = '<i class="icon fas fa-sun"></i>';
       } else if (theme.href.indexOf('light') > -1) {
-         if(dark.exists()) {
-            theme.href = './theme/dark.css';
-            changeThemeBtn.innerHTML = '<i class="icon fas fa-moon"></i>';
-         }
-      } else {
+         theme.href = './theme/dark.css';
+         changeThemeBtn.innerHTML = '<i class="icon fas fa-moon"></i>'
+      }else {
          console.error('Something must be wrong!');
       }
 
