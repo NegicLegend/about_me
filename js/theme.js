0ab1c7a3f08b9418ef7a3e360be361f262f61312
NegicLegend <88082310+NegicLegend@users.noreply.github.com>
2022-01-19 19:08:02 +0700

    add hover effect for toggle theme button

--- js/theme.js
+++ js/theme.js
@@ -5,7 +5,7 @@
    changeThemeBtn.onclick = function () {
       if (theme.href.indexOf('dark') > -1) {
          theme.href = './theme/light.css';
-         changeThemeBtn.innerHTML = '<i class="icon fas fa-sun"></i>'; 
+         changeThemeBtn.innerHTML = '<i class="icon fas fa-sun"></i>';
       } else if (theme.href.indexOf('light') > -1) {
          theme.href = './theme/dark.css';
          changeThemeBtn.innerHTML = '<i class="icon fas fa-moon"></i>';
